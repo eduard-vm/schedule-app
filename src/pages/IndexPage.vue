7c70a806537caa77b6832f1cee8ab148153adf99
Михалев Эдуард Викторович <eduard.mikhalev@sbermegamarket.ru>
2023-01-20 22:29:44 +0300

    работа на главной

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,5 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
     <QCalendarDay
       :weekdays="[1,2,3,4,5]"
       locale="ru"
